462c5bf816fdfbee2f815336ea2793e7fa74e98f
Torgy <jctorge@gmail.com>
2022-06-28 19:04:36 -0500

    roboco 2.0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.tlt').textillate()
+  
+  ScrollReveal({ 
+    reset: true,
+    origin: 'left',
+    viewOffset: {
+      top: 100,
+    },
+    beforeReveal: function beforeReveal(el) {
+      el.children[0].classList.add('animate__animated', 'animate__fadeInLeft')
+    },
+    afterReveal: function beforeReveal(el) {
+      el.classList.remove('animate__animated animate__fadeInLeft')
+    },
+  }).reveal('.reveal-left')
+
+  ScrollReveal({ 
+    reset: true,
+    origin: 'right',
+    viewOffset: {
+      top: 100,
+    },
+    beforeReveal: function beforeReveal(el) {
+      el.classList.add('animate__animated', 'animate__fadeInRight')
+    },
+    afterReveal: function beforeReveal(el) {
+      el.classList.remove('animate__animated','animate__fadeInRight')
+    },
+  }).reveal('.reveal-right')
+
+  particlesJS.load('hero', './js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+})
--- readme.md
+++ readme.md
@@ -1,8 +1,24 @@
 # RobotCo
 NOTE: This is not a real company this site is just for practice.
 
-Icons:
+## Local development
+`npx live-server`
+
+FontAwesome Icons:
 https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css
 
 Background Image from:
 https://www.deviantart.com/orginaljun/art/Free-Abstract-Background-Elegant-Tech-628772456
+
+Robots from https://onlinewebfonts.com
+
+Favicon created using [Real Favicon Generator](https://realfavicongenerator.net)
+
+## TODO:
+* https://animate.style/
+* https://kenwheeler.github.io/slick/
+* https://github.com/jlmakes/scrollreveal
+* https://letteringjs.com/
+* https://vincentgarreau.com/particles.js/
+* https://animejs.com
+* https://wicky.nillia.ms/headroom.js/
